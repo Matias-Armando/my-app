440032e408a53c6d55e403f2951bcdd99aa81388
Matias Armando <matiasarmando925@gmal.com>
2026-09-16 21:34:49 +0100

    speed vercel

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import {RouterProvider } from "react-router-dom"
 import { router } from "./routers"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 function App() {
   
 
   return (
     <>
+     <SpeedInsights />
       <RouterProvider router={router}/>
+      
     </> 
     
   )
